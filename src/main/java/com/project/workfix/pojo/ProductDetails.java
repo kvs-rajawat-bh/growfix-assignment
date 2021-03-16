b80ace08ca6f77c6760095ce4ad4f1299c81d583
unzipnav <unzip.nav@gmail.com>
2021-03-17 02:17:14 +0530

    created a pojo class to hold output of get request

---
+++ src/main/java/com/project/workfix/pojo/ProductDetails.java
@@ -0,0 +1,37 @@
+package com.project.workfix.pojo;
+
+public class ProductDetails {
+	
+	private double price;
+	
+	private String name;
+	
+	private String interest;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInterest() {
+		return interest;
+	}
+
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	
+	
+
+}
