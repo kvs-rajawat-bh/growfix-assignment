2c9444fa85524e734279eed1b678f39dde89a83e
unzipnav <unzip.nav@gmail.com>
2021-03-16 21:31:17 +0530

    starting project

---
+++ src/main/java/com/project/workfix/GrowfixAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.workfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowfixAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowfixAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/workfix/GrowfixAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowfixAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
